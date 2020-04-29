5064699ff837aac355c5b584904350e130f2944e
Jayaleshwari N <jayaleshwarin@syncfusion.com>
2020-04-29 14:50:33 +0530

    XAMARIN-34603 - Sample added.

---
+++ ListViewXamarin/ListViewXamarin/ListViewXamarin/Helper/Converter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace ListViewXamarin
+{
+    public class SwipeTemplateConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if ((string)parameter == "RightSwipeTemplate")
+            {
+                if ((FlowDirection)value == FlowDirection.RightToLeft)
+                    return App.Current.Resources["LeftSwipeTemplate"] as DataTemplate; 
+                else
+                    return App.Current.Resources["RightSwipeTemplate"] as DataTemplate;
+            }
+            else
+            {
+                if ((FlowDirection)value == FlowDirection.RightToLeft)
+                    return App.Current.Resources["RightSwipeTemplate"] as DataTemplate;
+                else
+                    return App.Current.Resources["LeftSwipeTemplate"] as DataTemplate;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
